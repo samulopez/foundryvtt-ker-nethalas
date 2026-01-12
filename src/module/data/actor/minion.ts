d3ab2a050400077bc35ebc76148d37417bbdfda1
Samuel Lopez <samulopez@gmail.com>
2026-01-12 13:34:47 +0100

    feat: add minions

--- src/module/data/actor/minion.ts
+++ src/module/data/actor/minion.ts
@@ -1,26 +1,9 @@
-import { attributeField } from './helper';
-import ActorDataModel from './actor';
+import MonsterDataModel, { defineMonsterModel } from './monster';
 
-import type { ActorDataModelSchema } from './actor';
+type MinionModelSchema = ReturnType<typeof defineMonsterModel>;
 
-const { NumberField, SchemaField } = foundry.data.fields;
-
-const defineMinionModel = () => ({
-  attributes: new SchemaField({
-    health: attributeField(1),
-    magicResistance: new NumberField({ required: true, integer: true, min: 0, initial: 20, max: 80 }),
-  }),
-  skills: new SchemaField({
-    endurance: new NumberField({ required: true, integer: true, min: 0, initial: 0, max: 100 }),
-    combat: new NumberField({ required: true, integer: true, min: 0, initial: 0, max: 100 }),
-  }),
-  // TODO: damage?
-});
-
-type MinionModelSchema = ReturnType<typeof defineMinionModel> & ActorDataModelSchema;
-
-export default class MinionDataModel extends ActorDataModel {
+export default class MinionDataModel extends MonsterDataModel {
   static defineSchema(): MinionModelSchema {
-    return { ...super.defineSchema(), ...defineMinionModel() };
+    return defineMonsterModel();
   }
 }
